6d2d8daece399c2397115f209e20b446c5917396
joogps <joogps@gmail.com>
2020-11-29 17:51:23 -0300

    Change stuff. Bad commit message.

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 let targets = [];
 let title;
+let titleOpacity = 255;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
-    noFill()
-    stroke(255)
-
+    
     title = loadImage('Title.png');
+
+    textAlign(CENTER);
 }
 
 function draw() {
     background(0);
 
-    if (targets.length < 1) {
-      image(title, width/2-title.width/4, height/2-title.height/4-50, title.width/2, title.height/2);
-      textAlign(CENTER);
-      text('toque em qualquer lugar para começar', width/2, height/2);
-    }
+    noStroke()
 
+    if (titleOpacity > 0.01) {
+      tint(255, titleOpacity)
+      image(title, width/2-title.width/4, height/2-title.height/4-50, title.width/2, title.height/2);
+
+      fill(255, titleOpacity)
+      textStyle(BOLD);
+      text('toque em qualquer lugar para começar', width/2, height/2);
+
+      if (!("vibrate" in window.navigator)) {
+        textStyle(ITALIC);
+        text('*navegador não suporta a função de vibração', width/2, height/2+100);
+      }
+
+      if (targets.length > 0) {
+        titleOpacity = lerp(titleOpacity, 0, 0.1)
+      }
+    }
+    
+    noFill()
     for (let i = 0; i < targets.length; i++) {
         for (let j = 0; j < targets.length; j++) {
           if (i != j && targets[i].checkIntersection(targets[j])) {
-            background(255-targets[i].diameter/12);
             if ("vibrate" in window.navigator) {
               window.navigator.vibrate(50);
             }
@@ -30,39 +44,52 @@ function draw() {
         }
         targets[i].display();
         targets[i].grow();
+        if (targets[i].opacity <= 0) {
+          targets.pop(i)
+        }
     }
 }
 
 function touchStarted() {
+  if (targets.length < 3) {
     targets.push(new Target(mouseX, mouseY))
     if ("vibrate" in window.navigator) {
       window.navigator.vibrate(50);
     }
+  }
 }
 
 class Target {
   constructor(x, y) {
     this.position = createVector(x, y)
     this.diameter = 0;
-    this.speed = 2;
-    this.thickness = 30;
+    this.speed = 2.5;
+    this.thickness = 50;
+    this.opacity = 255
   }
 
   grow() {
       this.diameter+= this.speed
+      this.speed+= 0.001
+      this.opacity-= this.speed/10
   }
 
   checkIntersection(target) {
-    if (this.position.dist(target.position) < this.diameter/2 + target.diameter/2 && 
-    (this.diameter/2 + target.diameter/2) % this.thickness/4 == 0) {
-      return true
+    if (this.position.dist(target.position) < this.diameter/2 + target.diameter/2) {
+      let intersection = (this.diameter/2 + target.diameter/2 - this.position.dist(target.position))%(this.thickness/4)
+      if (intersection > 0 && intersection < 5) {
+        return true
+      }
     }
   }
 
   display() {
-    strokeWeight(this.thickness/4)
-    stroke(255, 255-this.diameter/10)
+    stroke(255, this.opacity)
     for (let i = this.diameter; i > 0; i-= this.thickness) {
+        strokeWeight(this.thickness/4)
+        if (i-this.thickness <= 0) {
+          strokeWeight(this.thickness/4*(i/this.thickness))
+        }
         ellipse(this.position.x, this.position.y, i, i)
     }
   }
